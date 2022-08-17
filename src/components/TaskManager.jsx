7c541ada317d0c13bec06ed3cd3c35eb23065ae0
David Kwame Agyekum Dadzie <daviddadzie8@gmail.com>
2022-08-17 15:03:04 -0800

    how to implement useState

--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -1,53 +1,88 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
+
+import { v4 as uuid} from "uuid";
+
 import TaskItem from "./TaskItem";
+//import bg from "../assets/images/bg.png";
 
-function TaskManager(){
-const [tasks, setTasks] = useState([]);
-const [input, setInput] = useState("");
-
-const handleSubmit = (e) => {
-    e.preventDefault();
-    if(input==="") return;
-
-    setTasks([input, ...tasks]);
-    setInput("")
-};
-
-const handleDelete = idx => {
-    const newTask = tasks.filter((task) => task !== idx);
-    setTasks(newTask);
-
+const getTasksFromLocalStorage = () => {
+//get the task from the localstorage
+const saveTasks=localStorage.getItem("tasks");
+if (!saveTasks) return[];
+// We use of the parse method
+return JSON.parse(saveTasks);
 }
+function TaskManager (){
+    const [tasks, setTasks] = useState (getTasksFromLocalStorage);
+    const [input, setInput] = useState ("");
 
-    return (
- <div className="h-screen 
-         bg-blue-600 
-         flex justify-center
-         items-center" 
-         >
-            <div className="max-w-xl bg-white rounded-xl px-5 py-10">
-                <form  onSubmit={handleSubmit} className="space-x-5 w-[30rem] flex mb-10">
-                    <input type= "text" className="border-2
-                     border-green-400 p-2 rounded-md outline-none w-10/12" 
-                     onChange={(e) => setInput(e.target.value)}
-                    //  value={input === ""}
-                      />
-                     
-                    <button type="submit" className="bg-blue-600
-                     text-white text-lg py-2 px-5 rounded-md"
-                    disabled={input===""}
-                     > Add</button>
+    const handleSubmit = (e) =>{
+    
+        e.preventDefault();
+
+        if(input ==="") return;
+        const newTask = {
+            id: uuid(),
+            text: input,
+            completed: false
+
+        }
+        // const [input, setInput] = useState("");
+        // replace the input in the function below with newTask
+        // setTasks([input, ...tasks]);
+        setTasks([newTask, ...tasks]);
+        setInput("");
+
+        
+        
+
+    }
+    const handleDelete = id => {
+        const newTasks = tasks.filter((tasks) => tasks.id !== id);
+        setTasks(newTasks);
+        localStorage.setItem("task", JSON.stringify(tasks));
+    };
+
+    useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}, [tasks]);
+     return(
+
+        <div className="bg-blue-400 h-screen max-w-2xl w-full flex justify-center items-center">
+          
+            {/* <img src={bg} alt="background" className=" absolute w-full h-full object-cover mix-blend-screen "/> */}
+
+            <div className="max-w-xl rounded-xl bg-white py-10 px-5 max-h-96 overflow-hidden shadow-lg" >
+            <div className="flex justify-center mb-10 bg-blue-700 rounded-2xl py-3 shadow-md" >
+                <h1 className="text-white text-center text-lg font-extrabold ">DIARY</h1>
+            </div>
+                <form className="flex space-x-2 w-[30rem] shadow-md" onSubmit={handleSubmit}>
+
+               
+                    <input type="text" className="border-blue-400 border-2 px-2 py-3 rounded-md 
+                    p-2 outline-none w-10/12" placeholder="enter item " onChange={(e) =>setInput(e.target.value)} 
+                    value={input}/> 
+                <button  disabled ={input ===""} type="add" className="bg-blue-600 px-6 py-4 text-lg rounded-md text-center font-serif text-white shadow-md">Add</button>
                 </form>
+                <div className="mt-6 space-y-2 overflow-y-auto h-56 ">
+                 {
+                    tasks.map((task) =>(
+                    // <TaskItem task = {task} handleDelete = {handleDelete} />))
+                    <TaskItem key = {task.id} task = {task} handleDelete = {handleDelete} />))
 
-                <div className="space-y-3 overflow-auto h-50">
-                    {
-                    tasks.map((task) => (
-                    <TaskItem task={task} handleDelete={handleDelete} />
-                ))}
+                 }
+                        
+                
+                    
                 </div>
             </div>
-    </div>
+            
+                
+        
+        </div>
     );
+
 }
 
 export default TaskManager;
+
