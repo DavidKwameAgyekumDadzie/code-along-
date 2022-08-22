394374142520e53c7ccd1de30a3899773dcf606c
David Kwame Agyekum Dadzie <daviddadzie8@gmail.com>
2022-08-22 15:13:47 -0800

    how to use try catch

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,31 +1,39 @@
 import React, {useEffect, useState} from 'react'
 import Product from './Product'
 import axios from "axios"
+import useAxios from '../Hooks/useAxios';
+import Spinner from './Spinner';
+
 
 function ProductList() {
-    const [products, setProducts] = useState ([]);
-    useEffect(() => {
-        const getProducts = async ()  => {
-            const davids = await axios.get(
-                "https://api.escuelajs.co/api/v1/products"
-            );
-            // the console log is use to check whether what you did is correct in the browser
-            // console.log(davids)
-            setProducts(davids.data)
-        };
-        getProducts();
+    
+    const { data, isLoading, error } = useAxios("https://api.escuelajs.co/api/v1/products");
 
-    }, []); 
+    // useEffect(() => {
+    //     const getProducts = async ()  => {
+    //         const davids = await axios.get(
+    //             "https://api.escuelajs.co/api/v1/products"
+    //         );
+    //         // the console log is use to check whether what you did is correct in the browser
+    //         // console.log(davids)
+    //         setProducts(davids.data);
+    //     };
+    //     getProducts();
+
+    // }, []); 
+    // console.log(data)
+    if (isLoading) return <Spinner/>
+    if (error) return <p>{error}</p>
     return  (
 
-        <div className=' flex flex-wrap gap-2 pt-10 mt 5 justify-center items-center'>
-          {products.map((product) => (
-                 <Product item={product} />
+        <div className=' flex flex-wrap gap-2 pt-10 mt-5 justify-center items-center'>
+          {data.map((data) => (
+                 <Product item={data} />
           ))}
          
       </div>
-    )
-}
+    );
+};
 
 
 export default ProductList;
