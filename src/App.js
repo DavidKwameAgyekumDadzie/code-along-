90292f3d9236fcdf74959ac6796523b7f7377379
David Kwame Agyekum Dadzie <daviddadzie8@gmail.com>
2022-08-23 15:23:28 -0800

    brosing through links

--- src/App.js
+++ src/App.js
@@ -4,27 +4,33 @@
 // import Navbar from "./components/Navbar";
 // import Practice from "./components/Practice";
 import ProductList from "./components/ProductList";
-// import TaskManager from "./components/TaskManager";
+import TaskManager from "./components/TaskManager";
 // import Form from "./components/Form"
 // import Spinner from "./components/Spinner";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
-function App(){
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+import Navbar from "./components/Navbar";
+
+function App() {
   return (
-    <div>
-      {/* <Navbar />
-      <Header />
-      <Button label="Add to Cart" />
-      <Button label="Login" />
-      <Button label="Singup" />
-      <Button label="Register" />
-      <Image /> 
-      <Practice />  */}
-      {/* <TaskManager /> */}
-      {/* <Form /> */}
-      <ProductList/>
-      {/* <Spinner /> */}
-      
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/products" element={<ProductList />}/>
+        <Route path="/task-manager" element={<TaskManager />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact />}/>
+        
+        
+      </Routes>
+    </BrowserRouter>
+    
+  );
+  
+};
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,54 @@
+import React from "react"
 import Navitem from "./Navitem";
+import logo from "../assets/img/logo.jpeg"
+
+
+const links = [
+    {
+        label: "Home",
+        to: "/",
+    },
+
+    {
+        label: "Products",
+        to: "/products"
+    },
+
+    {
+        label: "Task Manager",
+        to: "/task-manager"
+    },
+
+    {
+        label: "About",
+        to: "/about"
+    },
+
+    {
+        label: "Contact",
+        to: "/contact"
+    }
+   
+]
 
 
 function Navbar (){
     return(
+        <div className='shadow-md py-7 px-5 bg-gradient-to-br from-green-400 to-pink-300'>
         <nav className="flex justify-between py-5 px-20 shadow-md">
-            <h3>Hostel Rental</h3>
+           <div className="flex justify-between gap-2">
+            <img className="w-10 h-10" src={logo} alt="sheflogo" />
+            <h1 className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-red-500 via-yellow-500 to-blue-500 ">SHeF Natural Clinic</h1>
+            </div>
             <ul className="flex gap-5">
-                <Navitem menu="Home" />
-                <Navitem menu="Product" />
-                <Navitem menu="Service" />
-                <Navitem menu="About" />
-                <Navitem menu="Contact" />
+                {links.map((link, index) => (
+                        <Navitem key={index} label={link.label} to={link.to} />
+                    ))
+                }
+               
             </ul>
         </nav>
+        </div>
     );
 }
 
